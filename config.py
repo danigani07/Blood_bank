390f900de20b6808ee9e4dd6dc3333c32034fb9a
danigani07 <danico07gamer@gmail.com>
2024-10-24 21:15:15 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    SECRET_KEY = 'your_secret_key'  # Use a strong secret key
+    MYSQL_HOST = '3306'  # Use '127.0.0.1' if necessary
+    MYSQL_USER = 'root'  # Your MySQL username
+    MYSQL_PASSWORD = 'mysql123'  # Your MySQL password
+    MYSQL_DB = 'blood_bankDB'  # Your MySQL database name
