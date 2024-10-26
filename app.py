5b9c34d2dd8b1d86d0c164a348140a036ca34855
danigani07 <danico07gamer@gmail.com>
2024-10-26 20:33:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class Config:
     MYSQL_HOST = 'bloodbank-db.clacek8ey7ty.us-east-1.rds.amazonaws.com'
     MYSQL_USER = 'admin'  # Your MySQL username
     MYSQL_PASSWORD = 'bloodbank12345'  # Your MySQL password
-    MYSQL_DB = 'bloodbank-db'  # Your MySQL database name
+    MYSQL_DB = 'bloodbank_db'  # Your MySQL database name
 
     # Add a DB_CONFIG attribute to hold database connection parameters
     DB_CONFIG = {
